d1e09ba0195f277899f8c33234fe01b3887b9ff2
obasoro <obasorokunle@gmail.com>
2024-01-05 08:57:11 -0600

    update repo in hamoye

---
+++ Terraform_storage/modules/services/webserver-cluster/variables.tf
@@ -0,0 +1,58 @@
+variable "server_port" {
+  description = "The port of the server"
+  type        = number
+  default     = 8080
+}
+
+variable "alb_name" {
+  description = "The name of the ALB"
+  type        = string
+  default     = "terraform-asg-webserver"
+
+}
+
+variable "instance_security_group_name" {
+  description = "The name of the security EC2 instance security"
+  type        = string
+  default     = "terraform-webserver-instance"
+
+}
+
+variable "alb_security_group_name" {
+  type        = string
+  description = "The name of the security group for the ALB"
+  default     = "terraform-webserver-alb"
+}
+
+variable "db_remote_state_bucket" {
+  description = "The Name of the remote state"
+  type = string
+
+}
+
+variable "db_remote_state_key" {
+  description = "The name of the key in the remote state"
+  type = string
+}
+
+variable "region" {
+  type = string
+  description = "Region where the remote state is stored"
+  default = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "The name to use for all the cluster resources"
+  type  = string
+}
+
+variable "db_remote_state_bucket" {
+  description = "the name of the bucket to use for all the remote state"
+  type = string
+}
+
+variable "db_remote_state_key" {
+  description = "the path to the key in the remote state"
+  type = string
+}
+
