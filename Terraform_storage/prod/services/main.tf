87501fd58b77df5648f4ef2e93d0637729364277
obasoro <obasorokunle@gmail.com>
2024-01-31 14:41:00 +0100

    update modules

--- Terraform_storage/prod/services/main.tf
+++ Terraform_storage/prod/services/main.tf
@@ -176,3 +176,24 @@ data "terraform_remote_state" "db" {
         region = "us-east-1"
     }
 }
+
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  scheduled_action_name = "scale-out-during-business-hours"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 10
+  recurrence = "0 9 * * *" # Start at 9:00 AM every day
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+    
+  }
+
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  scheduled_action_name = "scale-in-at-night"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 2
+  recurrence = "0 17 * *"
+}
